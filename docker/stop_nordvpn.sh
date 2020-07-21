1877b5b17be51f8d551ef655d7286ef2fd70b29b
Lieven Govaerts <lgo@mobsol.be>
2020-07-21 21:38:19 +0200

    docker: add stop script.

--- docker/stop_nordvpn.sh
+++ docker/stop_nordvpn.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker stack rm nordvpn
-docker container prune -f
+pushd docker
+pushd nordvpn
+MY_DOCKER_HOST=`ip -f inet addr show docker0 | awk '/inet/ {print $2}' | cut -d/ -f1` \
+  docker-compose stop
+popd
+popd
