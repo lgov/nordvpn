4f0ff16dda6158e65d8f21912241042288da8824
Lieven Govaerts <lgo@mobsol.be>
2020-07-21 17:54:50 +0200

    Add docker-compose, use docker service.

--- docker/run_nordvpn.sh
+++ docker/run_nordvpn.sh
@@ -1,8 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-echo "Stop any running nord_vpn container."
-docker stop nord_vpn | xargs -r docker rm
+# Prerequisites:
+# install docker compose
+# https://docs.docker.com/compose/install/
+# add aws creds for [image-ecr] in ~/.aws/credentials
 
-docker run -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --name nord_vpn \
-            --sysctl net.ipv4.conf.all.rp_filter=2 \
-            -e CONNECT="United States" -e TECHNOLOGY=NordLynx -d nordvpn
+# Make sure the ubuntu user's crontab has this line:
+# @reboot cd /home/ubuntu/dev/nordvpn && docker/run_nordvon.sh
+
+
+# Wait for the docker network to be online
+while ! ip -f inet addr show docker0; do
+  sleep 5
+done
+
+pushd docker
+echo "Sending docker-compose output to ~/docker_compose.log"
+
+echo "---------------------- STARTING ------------------------------" >> ~/docker_compose.log
+currentDate=`date`
+echo "Current time: $currentDate" >> ~/docker_compose.log
+
+pushd nordvpn
+MY_DOCKER_HOST=`ip -f inet addr show docker0 | awk '/inet/ {print $2}' | cut -d/ -f1` docker stack deploy --compose-file docker-compose.yml nordvpn >> ~/docker_compose.log 2>&1
+popd
+echo "-----------------------FINISHED ------------------------------" >> ~/docker_compose.log
+
+popd
